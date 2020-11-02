1505f9fb82c2f887f24fc2dd644667169d83afff
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2020-11-02 17:20:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,31 +12,35 @@ function setup() {
   bullet.velocityX = speed;
 
  wall = createSprite(1500,200,60,height/2);
- wall.Shapecolor = (80);
+ wall.shapecolor = color(80);
 }
 
 function draw() {
 
   background(0); 
-
-  if(wall.x-car.x < (car.width+wall.width)/2)
-{
-  bullet.velocityX = 0;
-  var deformation=0.5 * weight * speed * speed /22509;
-  if(deformation>180);
-  {
-    bullet.Shapecolor = color(255,0,0);
+  
+  if(hasCollided(bullet, wall)) {
+    bullet.velocityX=0; 
+    var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+    if(damage>10) {
+      wall.shapeColor=color(255,0,0); 
+                  } 
+    if(damage<10) {
+      wall.shapeColor=color(0,255,0);
+    }
   }
   
-    if (deformation <180 && deformation >100)
-  {
-  bullet.Shapecolor=color(230,230,0);
-  }
-  if (deformation>100)
-  {
-   bullet.Shapecolor=color(0,230,0);
-  }
+  drawSprites();
+  
 }
 
-  drawSprites();
+function hasCollided(lbullet, lwall) {
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallLeftEdge=lwall.x;
+  if (bulletRightEdge>=wallLeftEdge) { 
+    return true 
+  }
+  else{
+  return false; 
+}
 }
